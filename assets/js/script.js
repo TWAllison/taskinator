327ca6c526ca63f0e23c9d04e1532e4b979c01a8
TWAllison <tylerallison55@gmail.com>
2021-08-19 14:49:06 -0600

    get values from form add them using innerHTML property

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,20 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event) {
     event.preventDefault();
-    console.log(event);
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    //create the list Item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    //create a div to hole task info and add to the list item
+    var taskInfoEl = document.createElement("div");
+    //give it a class name 
+    taskInfoEl.className = "task-info";
+    //add html content inside the div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    //add the entire list item to the list
     tasksToDoEl.appendChild(listItemEl);
 };
 
