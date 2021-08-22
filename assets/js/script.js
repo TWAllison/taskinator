277728a854d409155a01964c8a581468e3a26315
TWAllison <tylerallison55@gmail.com>
2021-08-21 20:13:50 -0600

    dynamically add edit and delete buttons to task + select list items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
 
 
 var taskFormHandler = function (event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    //check if input values are empty strings
-    if (!taskNameInput || !taskTypeInput){
-        alert("You need to fill out the task form!");
+
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!"); //check if input values are empty strings
         return false;
     }
-        formEl.reset();
-    
+    formEl.reset();
+
 
     //package up data as an object
     var taskDataObj = {
@@ -26,18 +27,65 @@ var taskFormHandler = function (event) {
 
 var createTaskEl = function (taskDataObj) {
 
-    //create the list Item
-    var listItemEl = document.createElement("li");
+
+    var listItemEl = document.createElement("li"); //create the list item
     listItemEl.className = "task-item";
-    //create a div to hold task info and add to the list item
-    var taskInfoEl = document.createElement("div");
-    //give it a class name 
-    taskInfoEl.className = "task-info";
-    //add html content inside the div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.setAttribute = ("data-task-id", taskIdCounter);  //add task id as custom attribute
+
+    var taskInfoEl = document.createElement("div");  //create a div to hold task info and add to the list item
+    taskInfoEl.className = "task-info"; //give it a class name 
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";  //add html content inside the div
     listItemEl.appendChild(taskInfoEl);
-    //add the entire list item to the list
-    tasksToDoEl.appendChild(listItemEl);
+
+    var taskActionsEl = createTaskActions(taskIdCounter);
+    listItemEl.appendChild(taskActionsEl); // append taskActionEl to listItemEl before ListItemEl is appended to page 
+
+    tasksToDoEl.appendChild(listItemEl);  //add the entire list item to the list
+
+    taskIdCounter++; //increase taskcounter for next unique id
+};
+
+var createTaskActions = function (taskId) {
+    var actionsContainerEl = document.createElement("div");
+    actionsContainerEl.className = "task-actions";
+
+    var editButtonEl = document.createElement("button"); // create edit button
+    editButtonEl.textContent = "Edit";
+    editButtonEl.className = "btn edit-btn";
+    editButtonEl.setAttribute("data-task-id", taskId);
+
+    actionsContainerEl.appendChild(editButtonEl);
+
+    var deleteButtonEl = document.createElement("button");
+    deleteButtonEl.textContent = "Delete";
+    deleteButtonEl.className = "btn delete-btn";
+    deleteButtonEl.setAttribute("data-task-id", taskId);
+
+    actionsContainerEl.appendChild(deleteButtonEl);
+
+    var statusSelectEl = document.createElement("select");
+    statusSelectEl.className = "select-status";
+    statusSelectEl.setAttribute("name", "status-change");
+    statusSelectEl.setAttribute("data-task-id", taskId);
+
+    actionsContainerEl.appendChild(statusSelectEl);
+
+    var statusChoices = ["To DO", "In Progress", "completed"];
+
+    for (var i = 0; i < statusChoices.length; i++) {
+        //create option el
+        var statusOptionEl = document.createElement("options");
+        statusOptionEl.textContent = statusChoices[i];
+        statusOptionEl.setAttribute("value", statusChoices[i]);
+        //append to select
+        statusSelectEl.appendChild(statusOptionEl);
+    }
+
+
+
+    return actionsContainerEl;
+
+
 };
 
 
